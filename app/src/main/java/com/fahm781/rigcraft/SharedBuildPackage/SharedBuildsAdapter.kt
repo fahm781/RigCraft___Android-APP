9762d2394b5fc87b3e21dcdfcb1d35ab814bf24b
fahm781 <farazahmed9910@gmail.com>
2024-03-18 02:49:58 +0000

    Fixed Styling of the social hub. Added a dropdown so it doesn't take much space

--- app/src/main/java/com/fahm781/rigcraft/SharedBuildPackage/SharedBuildsAdapter.kt
+++ app/src/main/java/com/fahm781/rigcraft/SharedBuildPackage/SharedBuildsAdapter.kt
@@ -4,8 +4,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.fahm781.rigcraft.R
@@ -17,7 +21,17 @@ class SharedBuildsAdapter(var sharedBuilds: MutableList<SharedBuild>) : Recycler
         val buildName: TextView = view.findViewById(R.id.buildName)
         val sharedBy: TextView = view.findViewById(R.id.sharedBy)
         val productLayout: LinearLayout = view.findViewById(R.id.productLayout)
+//        val spinner: Spinner = view.findViewById(R.id.spinner)
 
+//        init {
+//            itemView.setOnClickListener {
+//                if (productLayout.visibility == View.GONE) {
+//                    productLayout.visibility = View.VISIBLE
+//                } else {
+//                    productLayout.visibility = View.GONE
+//                }
+//            }
+//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -30,27 +44,69 @@ class SharedBuildsAdapter(var sharedBuilds: MutableList<SharedBuild>) : Recycler
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val sharedBuild = sharedBuilds[position]
-        holder.buildName.text = "Build Name: " + sharedBuild.buildIdentifier //sets the build name to the buildIdentifier for now
+        holder.buildName.text =
+            "Build Name: " + sharedBuild.buildIdentifier //sets the build name to the buildIdentifier for now
         holder.sharedBy.text = "Shared by: " + sharedBuild.userEmail
 
-        // Add a TextView for each product in the build to the productLayout
+        addProductToLayout(sharedBuild, holder)
+
+        val spinnerButton: Button = holder.itemView.findViewById(R.id.spinnerButton)
+        spinnerButton.setOnClickListener {
+            if (holder.productLayout.visibility == View.VISIBLE) {
+                holder.productLayout.visibility = View.GONE
+            } else {
+                holder.productLayout.visibility = View.VISIBLE
+            }
+        }
+
+//        holder.spinner.setOnClickListener {
+//            if (holder.productLayout.visibility == View.VISIBLE) {
+//                holder.productLayout.visibility = View.GONE
+//            } else {
+//                holder.productLayout.visibility = View.VISIBLE
+//            }
+//        }
+//
+//        holder.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                holder.productLayout.visibility = View.GONE
+//            }
+//
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                view: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                holder.productLayout.visibility = View.GONE
+//            }
+//        }
+    }
+
+    private fun addProductToLayout(
+        sharedBuild: SharedBuild,
+        holder: SharedBuildsAdapter.ViewHolder
+    ) {
+        // Iterate over each product in the build
         for ((productType, productDetailsMap) in sharedBuild.buildData) {
             val productDetails = productDetailsMap as Map<String, Any>
 
+            // Inflate the layout for each product and find the views
             val productItemView = LayoutInflater.from(holder.itemView.context)
                 .inflate(R.layout.sharedproduct_item_layout, holder.productLayout, false)
-            val prodType : TextView = productItemView.findViewById(R.id.productType)
+            val prodType: TextView = productItemView.findViewById(R.id.productType)
             val titleTextView: TextView = productItemView.findViewById(R.id.titleTextView)
             val priceTextView: TextView = productItemView.findViewById(R.id.priceTextView)
             val productImageView: ImageView = productItemView.findViewById(R.id.productImageView)
 
+            // Set the text and image for each view
             prodType.text = productType
             titleTextView.text = productDetails["title"] as String
-            priceTextView.text = "£"+ productDetails["price"] as String
+            priceTextView.text = "£" + productDetails["price"] as String
             Picasso.get().load(productDetails["imageUrl"] as String).into(productImageView)
 
+            // Add the product layout to the productLayout
             holder.productLayout.addView(productItemView)
         }
-        Log.d("SharedBuildsAdapter", "onBindViewHolder: " + sharedBuild)
     }
 }
